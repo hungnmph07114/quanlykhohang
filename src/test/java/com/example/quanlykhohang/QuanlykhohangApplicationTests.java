051a51d43d1dda82f9c756fc4b6de87524739657
hungnmph07114 <itisloves2@gmail.com>
2024-07-18 19:23:10 +0700

    Thêm các tệp đầu tiên

---
+++ src/test/java/com/example/quanlykhohang/QuanlykhohangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlykhohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlykhohangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
